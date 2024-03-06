f64b405eb91b750c1e6ea8b95c7eff1cb83701ff
chaitanyarora <arorachaitany15@gmail.com>
2024-03-06 14:47:52 +0530

    Enabled Analytics_1

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ const App = () => (
 
 
   <div className="bg-primary w-full overflow-hidden scrollbarThin">
+  <Analytics />
     <BrowserRouter>
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
