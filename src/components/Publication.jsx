cf0abba9d62ace3b34bc5e69d67da24c759c2737
chaitanyarora <arorachaitany15@gmail.com>
2024-05-12 19:29:46 +0530

    Fixed

--- src/components/Publication.jsx
+++ src/components/Publication.jsx
@@ -15,7 +15,7 @@ const Publication = () => (
                     <li className='' id={p.id}>{p.content}</li>
                 ))}
                 <li>
-                    All papers (except pt.1) will be published in AIJR conference proceeding <a download="" className="font-medium text-blue-600 dark:text-blue-500 hover:underline" href="https://aijr.org/icamc-2024/">(https://aijr.org/icamc-2024/)</a> with ISBN number.
+                All papers (except pt.1) will be published in conference proceeding  <a download="" className="font-medium text-blue-600 dark:text-blue-500 hover:underline" href="https://aijr.org/icamc-2024/">(https://aijr.org/icamc-2024/)</a> with ISBN number/ISSN number with doi number and indexed in major database like crossref, BASE, Google Scholar e.t.c.
                 </li>
             </p>
         </div>
